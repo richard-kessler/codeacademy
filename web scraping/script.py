4d986582d23daa234bc97b8b504487b838d5b845
Richard Kessler <46903171+richard-kessler@users.noreply.github.com>
2022-06-22 16:11:08 -0400

    Update script.py

--- web scraping/script.py
+++ web scraping/script.py
@@ -4,3 +4,21 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Request
+webpage_response = requests.get("https://content.codecademy.com/courses/beautifulsoup/cacao/index.html")
+
+# Convert Request to Content
+webpage_content = webpage_response.content
+
+# Parse Content with bs
+soup = BeautifulSoup(webpage_content, "html.parser")
+print(soup)
+
+# Find all rating tags
+ratings_links = soup.find_all(attrs={"class": "Rating"})
+
+ratings = []
+for link in ratings_links:
+  ratings.append(float(link))
+
