260d8fd52b5dd36af33cdc1803e0105dbdca2d32
Richard Kessler <46903171+richard-kessler@users.noreply.github.com>
2022-06-23 14:57:32 -0400

    Update script with dataframe setup

--- web scraping/script.py
+++ web scraping/script.py
@@ -18,7 +18,38 @@ print(soup)
 # Find all rating tags
 ratings_links = soup.find_all(attrs={"class": "Rating"})
 
-ratings = []
-for link in ratings_links:
-  ratings.append(float(link))
+# Extract rate and convert to float value
+ratings = [float(rate.get_text()) for rate in ratings_links[1:]]
+#print(ratings)
 
+# Histogram to show ratings
+plt.hist(ratings)
+plt.show()
+
+# Find chocolatier companies
+all_choc = [choc.get_text() for choc in soup.select(".Company")[1:]]
+#print(all_choc)
+
+# Create Dataframe for all companies and ratings
+df = pd.DataFrame({"Company": all_choc, "Rating": ratings})
+#print(df)
+
+# Find Top 10 companies by rating
+mean_ratings = df.groupby("Company").Rating.mean()
+top_ten = mean_ratings.nlargest(10)
+#print(top_ten)
+
+#list of cocoa percentages
+cocoa_pct = [int(float(pct.get_text().strip("%"))) for pct in soup.select(".CocoaPercent")[1:]]
+#print(cocoa_pct)
+
+# Add cocoa_pct to existing dataframe
+df["CocoaPercentage"] = cocoa_pct
+#print(df)
+
+# Generate scatter plot of ratings vs % cocoa
+plt.scatter(df["CocoaPercentage"], df["Rating"], color="g")
+plt.title("Rating vs Cocoa %")
+z = np.polyfit(df.CocoaPercentage, df.Rating, 1)
+line_function = np.poly1d(z)
+plt.plot(df.CocoaPercentage, line_function(df.CocoaPercentage), "r--")
